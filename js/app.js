936326e27fa7d55405959a2b00d265c348298460
Lucas de Souza <lucassouzarocha1202@gmail.com>
2024-02-15 18:46:07 -0300

    Código refatorado!

--- js/app.js
+++ js/app.js
@@ -4,21 +4,32 @@ let jogosDevolvidos = [];
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let nomeDoJogo = gameClicado.querySelector(".dashboard__item__name").textContent;
-    let imagem = gameClicado.querySelector(".dashboard__item__img");
-    let botao = gameClicado.querySelector(".dashboard__item__button");
-    if(confirm(botao.classList.contains("dashboard__item__button--return"))) {
-        botao.classList.remove("dashboard__item__button--return");
-        imagem.classList.remove("dashboard__item__img--rented");
-        botao.textContent = "Alugar";
+    let imagemDoJogo = gameClicado.querySelector(".dashboard__item__img");
+    let botaoDoJogo = gameClicado.querySelector(".dashboard__item__button");
+
+    if(imagemDoJogo.classList.contains("dashboard__item__img--rented") && botaoDoJogo.classList.contains("dashboard__item__button--return")) {
+        imagemDoJogo.classList.remove("dashboard__item__img--rented");
+        botaoDoJogo.classList.remove("dashboard__item__button--return");
+        botaoDoJogo.textContent = "Alugar";
         jogosDevolvidos.push(nomeDoJogo);
         jogosAlugados.pop(nomeDoJogo);
         console.log(`Jogos devolvidos no momento: ${jogosDevolvidos.length}`);
-    } else {
-        botao.classList.add("dashboard__item__button--return");
-        imagem.classList.add("dashboard__item__img--rented");
-        botao.textContent = "Devolver";
-        jogosAlugados.push(nomeDoJogo);
+    }
+    else {
+        imagemDoJogo.classList.add("dashboard__item__img--rented");
+        botaoDoJogo.classList.add("dashboard__item__button--return");
+        botaoDoJogo.textContent = "Devolver";
         jogosDevolvidos.pop(nomeDoJogo);
+        jogosAlugados.push(nomeDoJogo);
         console.log(`Jogos alugados no momento: ${jogosAlugados.length}`);
     }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    exibirTotalJogosAlugadosEDevolvidos();
+})
+
+function exibirTotalJogosAlugadosEDevolvidos() {
+    let totalJogosAlugados = document.querySelectorAll(".dashboard__item__button--return").length;
+    console.log(`Começando com um total de jogos alugados: ${totalJogosAlugados}`);
 }
